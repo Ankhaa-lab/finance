4c471901cfe9dff3fce97f0ac658a442ae7facad
Ankhaa-lab <l.ankhaa1234@gmail.com>
2021-05-14 11:05:34 +0800

    Finance module ruu item add hiideg bolloo

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ var uiController = (function () {
   return {
     getInput: function () {
       return {
-        type: document.querySelector(DOMstrings.inputType).value,
-        description: document.querySelector(DOMstrings.inputDescription).value,
+        type: document.querySelector(DOMstrings.inputType).value, //inc, exp буюу +, - утгууд буцаана
+        description: document.querySelector(DOMstrings.inputDescription).value, //
         value: document.querySelector(DOMstrings.inputValue).value,
       };
     },
@@ -24,32 +24,57 @@ var uiController = (function () {
 })();
 
 // Санхүүтэй ажиллах контроллер
+
 var financeController = (function () {
   // байгуулагч функц ашиглаад обьект үүсгэе.
+  //private data
   var Income = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
+  //private data
   var Expense = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
 
-  var data ={
-    allItems: {
-      inc:[],
-      exp:[]
-    }
+  //private data
+  var data = {
+    items: {
+      inc: [],
+      exp: [],
+    },
     totals: {
       inc: 0,
-      exp: 0
-    }
-    
-    
-  }
+      exp: 0,
+    },
+  };
 
+  return {
+    addItem: function (type, desc, val) {
+      var item, id;
+      // id bol identification буюу тодорхойлж өгдөг зүйл, давтагдах ёсгүй.
+
+      if (data.items[type].length === 0) id = 1;
+      else {
+        id = data.items[type][data.items[type].length - 1].id + 1;
+      }
+
+      if (type === "inc") {
+        item = new Income(id, desc, val);
+      } else {
+        item = new Expense(id, desc, val);
+      }
+
+      data.items[type].push(item);
+    },
+
+    seeData: function () {
+      return data;
+    },
+  };
 })();
 
 // Программын холбогч конторл
@@ -58,8 +83,10 @@ var appController = (function (uiController, financeController) {
   var ctrlAddItem = function () {
     //  Click хийхэд хийгдэх үйлдэл.
     // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
-    console.log(uiController.getInput());
+    var input = uiController.getInput();
+    //console.log(uiController.data);
     // 2. Олж авсан өгөгдлүүдээ санхүүгийн контороллерт дамжуулж тэнд хадгална.
+    financeController.addItem(input.type, input.description, input.value);
     // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана.
     // 4. Төсвийг тооцоолно.
     // 5. Эцсийн үлдэгдэл, тооцог дэлгэцэнд гаргана.
