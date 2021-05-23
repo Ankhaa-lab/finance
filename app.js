85533e1554f4765f3573166a64e1ca1799bcefd9
Ankhaa-lab <l.ankhaa1234@gmail.com>
2021-05-23 10:35:32 +0800

    Sanhuugiin toolol hiilee

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // 63) Евент листенерээ тохируулъя, гарны эвенттэй ажиллах
+
 // Дэлгэцтэй холбоотой модулиа үүсгэе.
+// Дэлгэцтэй ажилладаг контроллер
 var uiController = (function () {
   // index.html файлын утгуудыг нэг доор бөөгнүүлэх, дараа өөрчлөлт хийхэд хялбар болгох үүднээс DOMstrings гэсэн обьект үүсгээд бөгнүүлэх кодыг дараах байдлаар бичлээ.
   var DOMstrings = {
@@ -7,18 +9,61 @@ var uiController = (function () {
     inputDescription: ".add__description",
     inputValue: ".add__value",
     addBtn: ".add__btn",
+    incomeList: ".income__list",
+    expenseList: ".expenses__list",
   };
   return {
     getInput: function () {
       return {
         type: document.querySelector(DOMstrings.inputType).value, //inc, exp буюу +, - утгууд буцаана
-        description: document.querySelector(DOMstrings.inputDescription).value, //
-        value: document.querySelector(DOMstrings.inputValue).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: parseInt(document.querySelector(DOMstrings.inputValue).value),
       };
     },
     // DOMstrings гэсэн хамгаалагдсан өгөгдлийг getDOMstrings функцээр дамжуулаад public болгож буцааж байна.
     getDOMstrings: function () {
       return DOMstrings;
+    },
+
+    clearFields: function () {
+      var fields = document.querySelectorAll(
+        DOMstrings.inputDescription + ", " + DOMstrings.inputValue,
+      );
+
+      // Convert List to Array
+      // fieldsArr хувьсагчид array обьектын prototype удамшилаар дамжуулан slice функцыг дуудаж байна.
+      // өөрөө хэлбэл var fieldsArr = new Array(); үүнтэй төстэй юм.
+      var fieldsArr = Array.prototype.slice.call(fields);
+
+      fieldsArr.forEach(function (el, index, array) {
+        el.value = "";
+      });
+      // дараах мөр код нь Массивын 0-р элементэд курсорыг шилжүүлж байна.
+      fieldsArr[0].focus();
+      // for (var i = 0; i < fieldsArr.length; i++) {
+      //   fieldsArr[i].value = "";
+      // }
+    },
+
+    addListItem: function (item, type) {
+      //Орлого зарлагын элементийг агуулсан html-ийг бэлтгэнэ.
+      var html, list;
+      if (type === "inc") {
+        list = DOMstrings.incomeList;
+        html =
+          '<div class="item clearfix" id="income-%id%"> <div class="item__description">$$DESCRIPTION$$</div>  <div class="right clearfix">   <div class="item__value">$$VALUE$$</div>  <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>  </div>  </div>  </div>';
+      } else {
+        list = DOMstrings.expenseList;
+        html =
+          '<div class="item clearfix" id="expense-%id%">    <div class="item__description">$$DESCRIPTION$$</div>  <div class="right clearfix">  <div class="item__value">$$VALUE$$</div>  <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>  </div>   </div> </div>';
+      }
+      // Тэр html дотроо орлого зарлагын утгуудыг REPLACE ашиглаж өөрчилж өгнө.
+      html = html.replace("%id%", item.id);
+      html = html.replace("$$DESCRIPTION$$", item.description);
+      html = html.replace("$$VALUE$$", item.value);
+
+      //Бэлтгэсэн HTML ээ DOM руу хийж өгнө.
+      document.querySelector(list).insertAdjacentHTML("beforeend", html);
     },
   };
 })();
@@ -40,6 +85,15 @@ var financeController = (function () {
     this.value = value;
   };
 
+  // Орлого тооцоололт
+  var calculateTotal = function (type) {
+    var sum = 0;
+    data.items[type].forEach(function (el) {
+      sum = sum + el.value;
+    });
+    data.totals[type] = sum;
+  };
+
   //private data
   var data = {
     items: {
@@ -50,9 +104,35 @@ var financeController = (function () {
       inc: 0,
       exp: 0,
     },
+
+    tusuv: 0,
+    huvi: 0,
   };
 
   return {
+    tusuvTootsooloh: function () {
+      // Нийт орлогын нийлбэрийг тооцоолох
+      calculateTotal("inc");
+
+      // Нийт зарлагын нийлбэрийг тооцоолох
+      calculateTotal("exp");
+
+      // ТӨсвйг шинээр тооцоолох
+      data.tusuv = data.totals.inc - data.totals.exp;
+
+      // Орлого зарлагын хувийг тооцоолно.
+      data.huvi = Math.round(data.totals.exp / data.totals.inc / 100);
+    },
+
+    tusuviigAvah: function () {
+      return {
+        tusuv: data.tusuv,
+        huvi: data.huvi,
+        totalsInc: data.totals.inc,
+        totalsExp: data.totals.exp,
+      };
+    },
+
     addItem: function (type, desc, val) {
       var item, id;
       // id bol identification буюу тодорхойлж өгдөг зүйл, давтагдах ёсгүй.
@@ -69,6 +149,8 @@ var financeController = (function () {
       }
 
       data.items[type].push(item);
+
+      return item;
     },
 
     seeData: function () {
@@ -85,11 +167,28 @@ var appController = (function (uiController, financeController) {
     // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
     var input = uiController.getInput();
     //console.log(uiController.data);
-    // 2. Олж авсан өгөгдлүүдээ санхүүгийн контороллерт дамжуулж тэнд хадгална.
-    financeController.addItem(input.type, input.description, input.value);
-    // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана.
-    // 4. Төсвийг тооцоолно.
-    // 5. Эцсийн үлдэгдэл, тооцог дэлгэцэнд гаргана.
+    if (input.description !== "" && input.value !== "") {
+      // 2. Олж авсан өгөгдлүүдээ санхүүгийн контороллерт дамжуулж тэнд хадгална.
+      var item = financeController.addItem(
+        input.type,
+        input.description,
+        input.value,
+      );
+      // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана.
+      uiController.addListItem(item, input.type);
+      uiController.clearFields();
+      // 4. Төсвийг тооцоолно.
+
+      financeController.tusuvTootsooloh();
+
+      // 5. Эцсийн үлдэгдэл, тооцог дэлгэцэнд гаргана.
+
+      var tusuv = financeController.tusuviigAvah();
+
+      // 6. Төсвийн тооцоог дэлгэцэнд гаргана.
+
+      console.log(tusuv);
+    }
   };
 
   //setupEventlisteners функц нь event -uudiig netgesen baidlaar байгуулж өгч байгаа private функц байна.
